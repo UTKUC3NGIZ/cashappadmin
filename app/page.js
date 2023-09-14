09fc7bd7f9a04131ce18258d846b23140e33d50d
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2023-09-14 14:17:13 +0300

    login admin

--- app/page.js
+++ app/page.js
@@ -1,23 +1,24 @@
-'use client';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import './globals.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+"use client";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import "./globals.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState({});
 
   const handlePress = () => {
     axios
       .post(
-        'https://mobil-bank-production.up.railway.app/swagger-ui/auth/login',
+        "https://mobil-bank-production.up.railway.app/auth/login",
         {
           email: email,
           password: password,
@@ -26,77 +27,87 @@ export default function Login() {
 
       .then(
         (response) => {
-          localStorage.setItem('userToken', response.data.token);
+          localStorage.setItem("userToken", response.data.token);
           console.log(response.data.token);
 
           if (response.data.token) {
-            navigation.navigate('HomePage');
+            navigation.navigate("addBalance");
           }
         },
 
         (error) => {
           console.log(error);
           if (!username.trim()) {
-            Alert.alert('Error', 'Please enter email!');
+            Alert.alert("Error", "Please enter email!");
           } else if (!password.trim()) {
-            Alert.alert('Error', 'Please enter password!');
+            Alert.alert("Error", "Please enter password!");
           } else {
-            Alert.alert( 'Make sure you entered the right parameters!');
+            Alert.alert("Make sure you entered the right parameters!");
           }
         }
       );
   };
+  useEffect(() => {
+    axios.get('https://mobil-bank-production.up.railway.app/users')
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error('Veri çekme hatası:', error);
+      });
+  }, []); 
+
   return (
-    <div className='flex align-items-center justify-content-center h-screen'>
-      <div className='surface-card p-4 shadow-2 border-round w-full lg:w-6'>
-        <div className='text-center mb-5'>
+    <div className="flex align-items-center justify-content-center h-screen">
+      <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+        <div className="text-center mb-5">
           <img
-            src='https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg'
-            alt='hyper'
-            className='mb-3 w-1/2 md:w-1/3 m-auto'
+            src="https://uploads-ssl.webflow.com/605c9d764f1ef938a009ac98/61e01bfbdd8632a72962edc2_Pinsoft_Yatay_Logo_mavi-for%20animation.svg"
+            alt="hyper"
+            className="mb-3 w-1/2 md:w-1/3 m-auto"
           />
-          <div className='text-900 text-3xl font-medium mb-3'>Giriş Yap</div>
+          <div className="text-900 text-3xl font-medium mb-3">Giriş Yap</div>
         </div>
 
         <div>
-          <label htmlFor='email' className='block text-900 font-medium mb-2'>
+          <label htmlFor="email" className="block text-900 font-medium mb-2">
             Email
           </label>
           <InputText
-            id='email'
-            type='text'
-            placeholder='utkucengiz00@gmail.com'
-            className='w-full mb-3'
+            id="email"
+            type="text"
+            placeholder="utkucengiz00@gmail.com"
+            className="w-full mb-3"
             value={email}
             onChange={(text) => {
               setEmail(text.target.value);
             }}
           />
 
-          <label htmlFor='password' className='block text-900 font-medium mb-2'>
+          <label htmlFor="password" className="block text-900 font-medium mb-2">
             Parola
           </label>
           <InputText
-            id='password'
-            type='password'
-            placeholder='******'
-            className='w-full mb-3'
+            id="password"
+            type="password"
+            placeholder="******"
+            className="w-full mb-3"
             value={password}
             onChange={(text) => {
               setPassword(text.target.value);
             }}
           />
 
-          <div className='flex align-items-center justify-content-end mb-6'>
-            <a className='font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer'>
+          <div className="flex align-items-center justify-content-end mb-6">
+            <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
               Parolamı Unuttum ?
             </a>
           </div>
 
           <Button
-            label='Giriş Yap'
-            icon='pi pi-user'
-            className='w-full'
+            label="Giriş Yap"
+            icon="pi pi-user"
+            className="w-full"
             onClick={handlePress}
           />
         </div>
